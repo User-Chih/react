ad37e8d2ba03b283d83cbb808d073d7f64da1b7f
unknown <ch1h@yandex.ru>
2020-06-26 19:03:45 +0300

    release v1

---
+++ 01-first-project/my-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+   return <header className="header grid">
+        <img src="https://i.ibb.co/Fxwk7gG/jsx-logo.png" alt="" />
+    </header>
+}
+
+export default Header;
---
+++ 01-first-project/my-app/src/components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Profile = () => {
+    return <div className="content">
+        {" "}
+        <div>
+            {/* <img src="https://i.ibb.co/VQdtDJb/jsxpng.png" alt="" /> */}
+        </div>
+        <div className="ava">AVA</div>
+        <div>My posts
+         <div>New post</div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
