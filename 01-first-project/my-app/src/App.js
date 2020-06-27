bce182b45a79c37cdbfec156ebc2da1429b87db1
unknown <ch1h@yandex.ru>
2020-06-28 00:27:58 +0300

    change and add files

--- 01-first-project/my-app/src/App.js
+++ 01-first-project/my-app/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import Profile from "./components/Profile";
+import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
     <div className="app-wrapper">
       <Header />
-      <Navbar />
       <Profile />
+      <Navbar />
+      <Footer />
     </div>
   );
 };
